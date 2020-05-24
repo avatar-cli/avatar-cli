df66688f929e1ae4e5810a410d13a45f4af9a5fc
Andres Correa Casablanca <castarco@coderspirit.xyz>
2020-05-25 00:35:58 +0200

    test: add basic pkg versions comparison to CI

--- cx/src/scripts/git_checks.ts
+++ cx/src/scripts/git_checks.ts
@@ -3,6 +3,34 @@
 import { getPlumberEnvVars } from '../lib/plumberEnv'
 import { fetch as gitFetch, getCommonAncestor, getCommitHashesList, checkIfSigned } from '../lib/git'
 import { execWithStringReturn } from '../lib/exec'
+import { getPackageJsonVersionFromCommit, getCargoTomlVersionFromCommit } from '../lib/version'
+
+async function checkCommitMessages(commonAncestor: string): Promise<void> {
+  console.log('Validating commit messages')
+  await execWithStringReturn(`npm run commitlint -- --from ${commonAncestor}`)
+}
+
+async function checkCommitSignatures(commitsList: string[]): Promise<void> {
+  console.log('Checking that commits are signed (but not verifying signatures)')
+  for (const commitHash of commitsList) {
+    if (!(await checkIfSigned(commitHash))) {
+      console.error(`ERROR: Commit ${commitHash} is not signed`)
+      process.exit(1)
+    }
+  }
+}
+
+async function checkVersions(currentGitRef: string): Promise<void> {
+  const packageJsonVersion = (await getPackageJsonVersionFromCommit(currentGitRef)).join('.')
+  const cargoTomlVersion = (await getCargoTomlVersionFromCommit(currentGitRef)).join('.')
+
+  if (packageJsonVersion !== cargoTomlVersion) {
+    console.error(
+      `ERROR: package.json version (${packageJsonVersion}) and Cargo.tom version (${cargoTomlVersion}) are different`
+    )
+    process.exit(1)
+  }
+}
 
 async function run(): Promise<void> {
   const env = await getPlumberEnvVars()
@@ -13,16 +41,9 @@ async function run(): Promise<void> {
   const commonAncestor = await getCommonAncestor(env.PLUMBER_GIT_MASTER_REF, env.CI_COMMIT_SHA)
   const commitsList = await getCommitHashesList(commonAncestor, env.CI_COMMIT_SHA)
 
-  console.log('Validating commit messages')
-  await execWithStringReturn(`npm run commitlint -- --from ${commonAncestor}`)
-
-  console.log('Checking that commits are signed (but not verifying signatures)')
-  for (const commitHash of commitsList) {
-    if (!(await checkIfSigned(commitHash))) {
-      console.error(`ERROR: Commit ${commitHash} is not signed`)
-      process.exit(1)
-    }
-  }
+  await checkCommitMessages(commonAncestor)
+  await checkCommitSignatures(commitsList)
+  await checkVersions(env.CI_COMMIT_SHA)
 }
 
 run()
