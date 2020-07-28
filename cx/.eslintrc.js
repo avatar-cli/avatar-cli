6e93cbe4c5bcc4824786d8ab2457ceac26eec1e0
Andres Correa Casablanca <castarco@coderspirit.xyz>
2020-07-28 13:17:49 +0200

    build: improve static checks on cx code

--- cx/.eslintrc.js
+++ cx/.eslintrc.js
@@ -1,24 +1,21 @@
+// NOTICE: VSCode will complain about this file due to a bug in its implementation.
+
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   plugins: ['@typescript-eslint', '@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
-    project: './tsconfig.json'
+    project: `${__dirname}/tsconfig.json`
   },
   rules: {
     '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        ignoreRestSiblings: true,
-      },
-    ],
+    '@typescript-eslint/no-unused-vars': 'error',
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'never'],
   },
