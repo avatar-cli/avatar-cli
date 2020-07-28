6c4199d0429b9bc0bd8698d0b2d0d9bf6be97102
Andres Correa Casablanca <castarco@coderspirit.xyz>
2020-07-28 13:54:13 +0200

    refactor: use more idiomatic code in prepare_commit_msg hook

--- cx/src/scripts/prepare_commit_msg.ts
+++ cx/src/scripts/prepare_commit_msg.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env ts-node-script
 
 import { existsSync } from 'fs'
+import { symlink, unlink } from 'fs/promises'
 import { execSync } from 'child_process'
 import { getCxEnvVars } from '../lib/cxEnv'
 
@@ -12,16 +13,21 @@ async function run(): Promise<void> {
   const env = await getCxEnvVars()
 
   if (existsSync(`${env.CX_PROJECT_DIR}/.git/rebase-merge`) || existsSync(`${env.CX_PROJECT_DIR}/.git/rebase-apply`)) {
+    // We skip the Commitizen wizard if we are in the middle of a rebase
     return
   }
 
-  execSync(`ln -fs "${env.CX_PROJECT_DIR}/.git" "${env.CX_PROJECT_DIR}/cx/.git"`)
+  const fakeGitDir = `${env.CX_PROJECT_DIR}/cx/.git`
+  await symlink(`${env.CX_PROJECT_DIR}/.git`, fakeGitDir)
   try {
     execSync('exec < /dev/tty && npm run git-cz -- --hook', { stdio: ['inherit', 'inherit', 'inherit'] })
   } catch {
     // Do nothing if it fails
   }
-  execSync('unlink ./.git')
+
+  if (existsSync(fakeGitDir)) {
+    await unlink(fakeGitDir)
+  }
 }
 
 run()
