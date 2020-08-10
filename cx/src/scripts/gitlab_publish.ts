eca891913f06ae528beae3c6511b0d3e26f84e3d
Andres Correa Casablanca <castarco@coderspirit.xyz>
2020-08-10 18:15:19 +0200

    ci: add crates.io link to generated releases

--- cx/src/scripts/gitlab_publish.ts
+++ cx/src/scripts/gitlab_publish.ts
@@ -5,22 +5,7 @@ import fetch from 'node-fetch'
 import { getCxEnvVars } from '../lib/cxEnv'
 import { getPackageJsonVersionFromCommit } from '../lib/version'
 
-async function run(): Promise<void> {
-  const env = await getCxEnvVars()
-
-  const gitRef = env.CX_GIT_COMMIT_HASH
-  const versionComponents = await getPackageJsonVersionFromCommit(gitRef)
-  const newTag = `v${versionComponents.join('.')}`
-
-  const ciProjectId = env.CI_PROJECT_ID ?? ''
-  if (ciProjectId === '') {
-    throw new Error('Project ID not defined')
-  }
-  const releaseToken = env.GITLAB_RELEASE_TOKEN ?? ''
-  if (releaseToken === '') {
-    throw new Error('CI token not defined')
-  }
-
+async function createNewTag(ciProjectId: string, newTag: string, gitRef: string, releaseToken: string) {
   const tagCreationResponse = await fetch(
     `https://gitlab.com/api/v4/projects/${ciProjectId}/repository/tags?tag_name=${newTag}&ref=${gitRef}`,
     {
@@ -36,7 +21,9 @@ async function run(): Promise<void> {
     console.error(`Tag Creation Response's Body:\n${await tagCreationResponse.text()}\n`)
     throw new Error('Error while creating new tag')
   }
+}
 
+async function createNewRelease(ciProjectId: string, newTag: string, releaseToken: string) {
   const releaseCreationResponse = await fetch(`https://gitlab.com/api/v4/projects/${ciProjectId}/releases`, {
     method: 'POST',
     body: JSON.stringify({
@@ -53,6 +40,57 @@ async function run(): Promise<void> {
     console.error(`Release Creation Response's Body:\n${await releaseCreationResponse.text()}\n`)
     throw new Error('Error while creating new release')
   }
+}
+
+async function createCratesIoLink(
+  ciProjectId: string,
+  newTag: string,
+  newVersion: string,
+  releaseToken: string
+): Promise<void> {
+  const cratesIoLinkResponse = await fetch(
+    `https://gitlab.com/api/v4/projects/${ciProjectId}/releases/${newTag}/assets/links`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        link_type: 'package',
+        name: 'Crates.io "Binary" Package',
+        url: `https://crates.io/crates/avatar-cli/${newVersion}`,
+      }),
+      headers: {
+        Authorization: `Bearer ${releaseToken}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+
+  if (!cratesIoLinkResponse.ok || cratesIoLinkResponse.status >= 300) {
+    console.error(`Crates.io Link Creation Response's HTTP Status:\n\r${cratesIoLinkResponse.status}\n`)
+    console.error(`Crates.io Link Creation Response's Body:\n${await cratesIoLinkResponse.text()}\n`)
+    throw new Error('Error while creating new Crates.io link')
+  }
+}
+
+async function run(): Promise<void> {
+  const env = await getCxEnvVars()
+
+  const gitRef = env.CX_GIT_COMMIT_HASH
+  const versionComponents = await getPackageJsonVersionFromCommit(gitRef)
+  const newVersion = versionComponents.join('.')
+  const newTag = `v${newVersion}`
+
+  const ciProjectId = env.CI_PROJECT_ID ?? ''
+  if (ciProjectId === '') {
+    throw new Error('Project ID not defined')
+  }
+  const releaseToken = env.GITLAB_RELEASE_TOKEN ?? ''
+  if (releaseToken === '') {
+    throw new Error('CI token not defined')
+  }
+
+  await createNewTag(ciProjectId, newTag, gitRef, releaseToken)
+  await createNewRelease(ciProjectId, newTag, releaseToken)
+  await createCratesIoLink(ciProjectId, newTag, newVersion, releaseToken)
 }
 
 run()
